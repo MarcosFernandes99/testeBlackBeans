215c2a08c3ed24dd8ee22e953f5b1473c21c4c1c
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2024-06-04 21:49:24 -0300

    feat: estado hover adicionado

--- home.js
+++ home.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonCabeçalho = document.querySelectorAll(".opções");
 
+  buttonCabeçalho.forEach((button) => {
+    button.addEventListener("mouseover", () => {
+      button.classList.add("opções-hover");
+    });
+
+    button.addEventListener("mouseout", () => {
+      button.classList.remove("opções-hover");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonMenu = document.querySelectorAll(".menu-opções");
+
+  buttonMenu.forEach((button) => {
+    button.addEventListener("mouseover", () => {
+      button.classList.add("menu-opções-hover");
+    });
+
+    button.addEventListener("mouseout", () => {
+      button.classList.remove("menu-opções-hover");
+    });
+  });
+});
